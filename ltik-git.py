c1df5eeee02160382edc0b53d5525c636a4e09f8
sitimasda15 <mazdasiti15@gmail.com>
2024-11-10 15:00:21 +0700

    Update ltik-git.py

--- ltik-git.py
+++ ltik-git.py
@@ -2,3 +2,9 @@ nilai = int(input("Masukkan nilai: "))
 
 if (nilai >= 90):
     print("merupakan nilai berpredikat A")
+elif(nilai < 90 and nilai >= 80):
+    print("merupakan nilai berpredikat B")
+elif(nilai < 80 and nilai >= 70):
+    print("merupakan nilai berpredikat C")
+elif(nilai < 70):
+    print("merupakan nilai berpredikat D")
